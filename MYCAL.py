e9c346fb5cc677426e9b4b01618098d9b2af81e6
Zarcotech <108834196+Zarcotech@users.noreply.github.com>
2022-07-06 14:43:15 -0500

    Add files via upload

---
+++ MYCAL.py
@@ -0,0 +1,23 @@
+what=input("What process type do you want?(+,-,*,/)")
+if what == "+":
+    n1=input("What is your first number you want to add?")
+    n2=input("What is your second number you want to add?")
+    print(int(n1)+int(n2))
+else:
+    if what == "-":
+        n1=input("What is your first number you want to add?")
+        n2=input("What is your second number you want to add?")
+        print(int(n1)-int(n2))
+    else:
+        if what == "*":
+            n1=input("What is your first number you want to add?")
+            n2=input("What is your second number you want to add?")
+            print(int(n1)*int(n2))
+        else:
+            if what == "/":
+                n1=input("What is your first number you want to add?")
+                n2=input("What is your second number you want to add?")
+                print(int(n1)/int(n2))
+            
+    
+    
